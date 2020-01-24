33b1c36ac9e2b6ed17fabe2a7b6fec4b53c11923
mishta-real <mikado.brave@gmail.com>
2020-01-24 18:33:44 +0700

    Removed unused functions in filter, etc.

--- reset_usb.c
+++ reset_usb.c
@@ -18,31 +18,28 @@
 
 int main(int argc, char **argv)
 {
-    const char *filename;
-    int fd;
-    int rc;
-
     if (argc != 2) {
-        fprintf(stderr, "Usage: usbreset device-filename\n");
+        fprintf(stderr, "Usage: usbreset /dev/bus/usb/<bus-number>/<device-number>\n");
         return 1;
     }
 
-    filename = argv[1];
+    const char *filename = argv[1];
 
-    fd = open(filename, O_WRONLY);
+    int fd = open(filename, O_WRONLY);
     if (fd < 0) {
-        perror("Error opening output file");
+        perror("Error opening output file.");
         return 1;
     }
 
-    printf("Resetting USB device %s\n", filename);
-    rc = ioctl(fd, USBDEVFS_RESET, 0);
+    printf("Resetting USB device %s ...\n", filename);
+
+    int rc = ioctl(fd, USBDEVFS_RESET, 0);
     if (rc < 0) {
-        perror("Error in ioctl");
+        perror("Error in ioctl.");
         return 1;
     }
 
-    printf("Reset successful\n");
+    printf("Reset successful.\n");
 
     close(fd);
 
